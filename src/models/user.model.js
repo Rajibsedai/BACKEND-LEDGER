e9040027e180f6bd2fccf1b182945b9770db102a
Rajib <rajibsedhai@gmail.com>
2026-08-01 21:14:31 +0545

    feat: implement user login functionality with JWT authentication

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -34,7 +34,7 @@ userSchema.pre("save",async function(){
 })
 
 userSchema.methods.comparePassword = async function (password){
-    return await bcrypt.compare(password. this.password)
+    return await bcrypt.compare(password, this.password)
 }
 
 const userModel = mongoose.model("user",userSchema)
